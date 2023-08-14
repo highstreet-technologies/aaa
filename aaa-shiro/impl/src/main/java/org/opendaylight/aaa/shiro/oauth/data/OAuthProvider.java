3795315313a29b67aa7abc10ac2e87131031bfd7
Michael Dürre <michael.duerre@highstreet-technologies.com>
2023-08-14 06:41:46 +0200

    add files for oauth2realm to load

---
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/oauth/data/OAuthProvider.java
@@ -0,0 +1,5 @@
+package org.opendaylight.aaa.shiro.oauth.data;
+
+public enum OAuthProvider {
+    KEYCLOAK, NEXTCLOUD, GITLAB
+}
---
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/oauth/data/UnableToConfigureOAuthService.java
@@ -0,0 +1,12 @@
+package org.opendaylight.aaa.shiro.oauth.data;
+
+public class UnableToConfigureOAuthService extends Exception {
+
+    public UnableToConfigureOAuthService(String configUrl){
+        super(String.format("Unable to configure OAuth service from url %s", configUrl));
+    }
+    public UnableToConfigureOAuthService(String configUrl, int responseCode){
+        super(String.format("Unable to configure OAuth service from url %s. bad response with code %d", configUrl, responseCode));
+    }
+
+}
